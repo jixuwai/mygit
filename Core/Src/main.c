2a2780b830f42bc6dc867d5ea2229fb272a94add
jixuwai <2855027651@qq.com>
2026-07-28 14:43:25 +0800

    Add HAL_Delay to main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,6 +99,7 @@ int main(void)
   OLED_ShowString(1, 3, "video");
   OLED_ShowString(2, 3, "music");
   OLED_ShowString(3, 3, "cnt:");
+  HAL_Delay(10);
   /* USER CODE END 2 */
 
   /* Infinite loop */
